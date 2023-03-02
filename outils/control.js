75b26b98ad9d71bb4ca6a0662c1a95e60d5a3cae
RedShip <42253239+TheRedShip@users.noreply.github.com>
2023-03-02 21:14:40 +0100

    Update control.js

--- outils/control.js
+++ outils/control.js
@@ -1,7 +1,37 @@
-import { toggleTab } from "../uiScript.js"
+import { toggleTab,subSelected } from "../uiScript.js"
+import { drawLines, drawText,dist } from "../utils.js"
+import { Colors } from "../constant.js"
+import { currentZoom } from "../camera.js"
 
-function controlLoop(obj){
-    if(obj.physics) toggleTab(obj)
+
+function controlLoop(c,obj,mousepos,currentBuild){
+    if(subSelected==0){ 
+        if(obj != -1 && obj.physics) toggleTab(obj) 
+        return {oldPos:{x:0,y:0}, obj:-1}
+    }
+    else if(subSelected==1){
+        let oldPos = currentBuild.oldPos
+
+
+        let textPos = {x:oldPos.x+(mousepos.x-oldPos.x)/2,y:oldPos.y+(mousepos.y-oldPos.y)/2}
+        let textSize = Math.max(100,100*(currentZoom**2));
+        let distance = dist(oldPos.x,mousepos.x,oldPos.y,mousepos.y)
+
+        drawText(c,textPos.x,textPos.y,textSize,String(distance.toFixed(0)) + "px")
+        drawLines(c,oldPos.x,oldPos.y,mousepos.x,mousepos.y,Colors.measure,10,[10,10])
+
+        
+        return currentBuild
+    }else{
+
+    }
+    
 }
 
-export { controlLoop }
+function controlSecondClick(){
+    if(subSelected==1){
+        return {oldPos:{x:0,y:0}, obj:-1}
+    }
+}
+
+export { controlLoop,controlSecondClick }
